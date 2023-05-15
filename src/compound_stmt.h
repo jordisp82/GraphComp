2a890e8e0532e58c10c2e02fb6486f03d9fc8928
Jordi Salvador Pont <jordisalvadorpont@gmail.com>
2023-05-15 08:46:28 +0200

    Reorg in progress

--- src/compound_stmt.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMPOUND_STMT_H
-#define COMPOUND_STMT_H
-
-#include "nonterms.h"
-#include "ast_t.h"
-
-compound_stmt_t *sem_compound_stmt (ast_node_t * ast);
-
-#endif
--- src/statement.h
+++
@@ -1,9 +0,0 @@
-#ifndef STATEMENT_H
-#define STATEMENT_H
-
-#include "nonterms.h"
-#include "ast_t.h"
-
-statement_t *sem_statement (ast_node_t * ast);
-
-#endif
