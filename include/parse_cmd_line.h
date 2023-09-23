8bac23d20eb1206754db7e4102783826d5317f3b
Jordi Salvador Pont <jordisalvadorpont@gmail.com>
2023-09-23 15:55:50 +0200

    Fuckoff

--- include/parse_cmd_line.h
+++
@@ -1,8 +0,0 @@
-#ifndef PARSE_CMD_LINE_H
-#define PARSE_CMD_LINE_H
-
-#include "project_t.h"
-
-int parse_cmd_line (int argc, char *argv[], project_t * project);
-
-#endif
--- include/preproc.h
+++
@@ -1,9 +0,0 @@
-#ifndef PREPROC_H
-#define PREPROC_H
-
-#include "project_t.h"
-
-int preproc (const char *src, char **dst, const ipath_t * paths,
-             const macro_t * macros);
-
-#endif
